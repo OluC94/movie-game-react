eb0d62c3b646f2038101af08921936b698b9844d
OluC94 <coker.olu94@gmail.com>
2022-11-19 12:23:52 +0000

    complete setup for fetching actor info

--- movie-game/src/components/Game.jsx
+++ movie-game/src/components/Game.jsx
@@ -2,21 +2,40 @@ import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
 import { getActorFilmography, getBio, getMostPopular } from "../utils/api";
+import { Loading } from "./Loading";
 
 export const Game = () => {
   const [firstTest, setFirstTest] = useState(false);
   const [startActor, setStartActor] = useState({});
+  const [endActor, setEndActor] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     setFirstTest(false);
-    // const test = getBio("nm6073955");
-    setFirstTest(true);
-  }, []);
+    setIsLoading(true);
+    const start = getBio("nm6073955");
+    const target = getBio("nm5939164");
 
+    Promise.all([start, target]).then((actors) => {
+      setStartActor(actors[0]);
+      setEndActor(actors[1]);
+      setIsLoading(false);
+      setFirstTest(true);
+    });
+  }, []);
+  if (isLoading) return <Loading />;
   return (
     <section className="game-page">
-      <section>game page</section>
-      <section>{firstTest ? "true" : "false"}</section>
+      <h2>game page</h2>
+      <section>
+        {startActor.name}
+        <img className="actor-img" src={startActor.img} />
+      </section>
+      <section>
+        {endActor.name}
+        <img className="actor-img" src={endActor.img} />
+      </section>
+      <section>{firstTest ? "useEffect running" : "false"}</section>
     </section>
   );
 };
